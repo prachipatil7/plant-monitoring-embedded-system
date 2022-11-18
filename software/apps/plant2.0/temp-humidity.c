40555cf0d345d4d902baa90bdc78bc08fb7a6098
Elena Fabian <elenafabian@dhcp-10-105-200-188.wireless.northwestern.private>
2022-11-18 14:12:32 -0600

    in prog

--- software/apps/plant2.0/temp-humidity.c
+++ software/apps/plant2.0/temp-humidity.c
@@ -20,6 +20,17 @@ void shtc3_init(const nrf_twi_mngr_t* i2c) {
   i2c_manager = i2c;
 
   // Read unique shtc3 ID - should be same every time
-  uint8_t whoami = i2c_reg_write(TEMP_MOISTURE_SENSOR, READ_ID);
-  printf("WHO AM I? temp-mosture sensor: %x\r\n", whoami);
+  // I2C ADDRESS = 0x70
+  // READ_ID = 0xEFC8
+  uint8_t p_data[] = {0xEF, 0xC8};
+  nrf_twi_mngr_transfer_t const write_transfer[] = {
+     NRF_TWI_MNGR_WRITE((uint8_t) 0x70,
+			&p_data,
+			2,
+			0)				  						   
+  };
+  nrf_twi_mngr_perform(i2c_manager, NULL, write_transfer, 2, NULL);
+
+  uint8_t whoami = i2c_reg_write((uint8_t) 0x70, (uint8_t) 0x70, uint8_t data);
+  printf("WHO AM I? temp-humidity sensor: %x\r\n", whoami);
 }
