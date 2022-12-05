22cdf7a202782288230dcb20d0de0c413f3900ca
gedbe02 <82680052+gedbe02@users.noreply.github.com>
2022-12-04 20:20:33 -0600

    Update led_driver.h

--- software/apps/plant2.0/led_driver.h
+++ software/apps/plant2.0/led_driver.h
@@ -12,8 +12,20 @@ typedef enum {
    LED_DEV_ADDR = 0x40,
    LED_MODE1_REG = 0x00,
    LED_PRESCALER_REG = 0xFE,
+
+   //LEDs
+   LED4 = 0x16,
+   LED5 = 0x1a,
+   LED6 = 0x1e,
+   LED7 = 0x22,
+   LED8 = 0x26,
+
+
 } led_reg_t;
 
 void led_init(const nrf_twi_mngr_t *i2c);
+void light_on(float r, float g, float b);
+void adjust_led_brightness(uint16_t *buf);
+
 
 
