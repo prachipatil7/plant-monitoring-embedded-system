36518a9e22a494e8d1760eb36dda2778f5abefd0
Elena Fabian <86376792+elenafabian@users.noreply.github.com>
2022-12-06 21:47:26 -0600

    Cleaned

--- software/apps/plant2.0/spectral.c
+++ software/apps/plant2.0/spectral.c
@@ -15,51 +15,51 @@ uint8_t read_spectral_whoami(void) {
 
 void disable_all_smux_channels(void) {
   for (uint16_t i=0; i < 0x14; i++) {
-     i2c_reg_write(SPECT_DEV_ADDR, i, 0x0);
+    i2c_reg_write(SPECT_DEV_ADDR, i, 0x0);
   }
 }
 
 void setup_channel_f1_left(void) {
-   disable_all_smux_channels();
-   i2c_reg_write(SPECT_DEV_ADDR, 0x01, 0x01);
+  disable_all_smux_channels();
+  i2c_reg_write(SPECT_DEV_ADDR, 0x01, 0x01);
 }
 
 void setup_low_channels() {
-   disable_all_smux_channels();
-   i2c_reg_write(SPECT_DEV_ADDR, 0x00, 0x30);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x01, 0x01);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x05, 0x42);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x08, 0x50);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x0C, 0x20);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x0D, 0x04);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x0F, 0x30);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x10, 0x01);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x11, 0x50);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x13, 0x06);
+  disable_all_smux_channels();
+  i2c_reg_write(SPECT_DEV_ADDR, 0x00, 0x30);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x01, 0x01);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x05, 0x42);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x08, 0x50);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x0C, 0x20);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x0D, 0x04);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x0F, 0x30);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x10, 0x01);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x11, 0x50);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x13, 0x06);
 }
 
 void setup_high_channels() {
-   disable_all_smux_channels();
-   i2c_reg_write(SPECT_DEV_ADDR, 0x03, 0x40);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x04, 0x02);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x06, 0x10);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x07, 0x03);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x08, 0x50);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x09, 0x10);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x0A, 0x03);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x0E, 0x24);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x11, 0x50);
-   i2c_reg_write(SPECT_DEV_ADDR, 0x13, 0x06);
+  disable_all_smux_channels();
+  i2c_reg_write(SPECT_DEV_ADDR, 0x03, 0x40);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x04, 0x02);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x06, 0x10);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x07, 0x03);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x08, 0x50);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x09, 0x10);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x0A, 0x03);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x0E, 0x24);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x11, 0x50);
+  i2c_reg_write(SPECT_DEV_ADDR, 0x13, 0x06);
 }
 
 void enable_spectral_measurement(bool enable) {
-   i2c_change_bit(SPECT_DEV_ADDR, SPECT_ENABLE_REG, 1, (int)enable);
+  i2c_change_bit(SPECT_DEV_ADDR, SPECT_ENABLE_REG, 1, (int)enable);
 }
 
 void set_smux_low(bool f1_f4) {
-   enable_spectral_measurement(false);
-   i2c_reg_write(SPECT_DEV_ADDR, SPECT_SMUX_CFG_REG, 2<<3); // set to write
-   if (f1_f4) {
+  enable_spectral_measurement(false);
+  i2c_reg_write(SPECT_DEV_ADDR, SPECT_SMUX_CFG_REG, 2<<3); // set to write
+  if (f1_f4) {
     setup_low_channels();
   } else {
     setup_high_channels();
@@ -79,14 +79,14 @@ void delay_for_data() {
 }
 
 uint16_t read_channel(uint8_t channel) {
-   uint8_t lsb = i2c_reg_read(SPECT_DEV_ADDR, SPECT_CH0_REG_L + 2 * channel);
-   uint16_t msb = (uint16_t) i2c_reg_read(SPECT_DEV_ADDR, SPECT_CH0_REG_H + 2 * channel);
-   return (msb << 8) | lsb;
+  uint8_t lsb = i2c_reg_read(SPECT_DEV_ADDR, SPECT_CH0_REG_L + 2 * channel);
+  uint16_t msb = (uint16_t) i2c_reg_read(SPECT_DEV_ADDR, SPECT_CH0_REG_H + 2 * channel);
+  return (msb << 8) | lsb;
 }
 
 void read_status() {
   uint8_t status = i2c_reg_read(SPECT_DEV_ADDR, SPECT_ASTATUS_REG);
-  printf("   ASTATUS: %d\n", status);
+  printf("ASTATUS: %d\n", status);
 }
 
 void read_spectral_all_channels(uint16_t *buf) {
@@ -94,17 +94,17 @@ void read_spectral_all_channels(uint16_t *buf) {
   enable_spectral_measurement(true);
   delay_for_data();
   uint8_t buf_index = 0;
-  
+
   //read low channels
   for (uint8_t channel=0; channel<4; channel++) {
     buf[buf_index] = read_channel(channel);
     buf_index++;
   }
-  
+
   set_smux_low(false);
   enable_spectral_measurement(true);
   delay_for_data();
-  
+
   //read high channels
   for (uint8_t channel=0; channel<6; channel++) {
     buf[buf_index] = read_channel(channel);
